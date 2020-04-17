993e3b492fe2add81c7dc1ad4ce0d7a3f0cf9667
kylin <kylin@kylindeMacBook-Pro.local>
2020-04-17 13:32:19 +0800

    消息列表项

--- app/screens/message.js
+++ app/screens/message.js
@@ -1,14 +1,74 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 class Message extends Component {
   render() {
     return (
-      <View>
-        <Text>message</Text>
+      <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={{uri: 'https://cdn.huodao.hk/zlj_default_ava.png'}}
+        />
+        <View style={styles.middleContent}>
+          <Text style={styles.title}>腾讯看点</Text>
+          <Text style={styles.desc}>[精选]今日步数到</Text>
+        </View>
+        <View style={styles.rightContent}>
+          <Text style={styles.time}>12:21</Text>
+          <View style={styles.circle}>
+            <Text style={styles.total}>6</Text>
+          </View>
+        </View>
       </View>
     );
   }
 }
 
 export default Message;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    height: 60,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 30,
+    height: 30,
+    marginLeft: 10,
+  },
+  middleContent: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    marginLeft: 10,
+  },
+  title: {
+    fontSize: 14,
+  },
+  desc: {
+    fontSize: 10,
+    color: 'gray',
+  },
+  rightContent: {
+    alignItems: 'center',
+    marginRight: 5,
+  },
+  time: {
+    color: 'gray',
+    fontSize: 12,
+  },
+  circle: {
+    width: 15,
+    height: 15,
+    borderRadius: 12,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  total: {
+    color: 'white',
+    fontSize: 10,
+  },
+});
